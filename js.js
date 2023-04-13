486d61c50793d167fe1ffa2bf53fc7af63e03b7d
SamiOMGgurl <83353299+SamiOMGgurl@users.noreply.github.com>
2023-04-13 18:58:08 +0400

    Add files via upload

---
+++ js.js
@@ -0,0 +1,21 @@
+function preload() 
+{
+}
+
+function setup() {
+    canvas = createCanvas(640,480);
+    canvas.position(300,250);
+    video = createCapture(VIDEO);
+    video.hide();
+
+    tint_color = "";
+}
+
+function draw() {
+    image(video, 0, 0, 640, 480);
+    tint(tint_color);
+}
+
+function apply() {
+    tint_color = document.getElementById("color_name").value;
+}
